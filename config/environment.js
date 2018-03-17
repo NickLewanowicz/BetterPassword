5908ed2af19ff27d265bcfabf5887dea68e785a5
Nicholas Lewanowicz <nlewanowicz@accedian.com>
2018-03-16 21:48:15 -0400

    Added ember-cli-github-pages addon

--- config/environment.js
+++ config/environment.js
@@ -45,6 +45,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.baseUrl = '/ledger-password/';
     // here you can enable a production-specific feature
   }
 
