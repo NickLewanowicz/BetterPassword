991319be38da21f73b29fc0cac3fd3fe2cd86710
Nicholas Lewanowicz <nlewanowicz@accedian.com>
2018-03-19 17:37:40 -0400

    adding fb support

--- app/pods/log/route.js
+++ app/pods/log/route.js
@@ -1,5 +1,5 @@
 import Route from '@ember/routing/route';
-
+import moment from 'moment'
 import $ from 'jquery';
 import RSVP from 'rsvp';
 
@@ -7,6 +7,11 @@ export default Route.extend({
   model () {
     return RSVP.hash({
       events: $.getJSON('/api/events')
+    }).then(events => {
+      this.store.createRecord('log', {
+        id: moment.now(),
+        dataset:events
+      }).save()
     });
   }
 });
